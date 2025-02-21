c209f8fe9e6721107f7d3ae334f0f374d433ddc8
Emil Skog <emil@skog.io>
2025-02-21 17:05:47 +0100

    Chapter 1 complete

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
+
+func returnHealth(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type:", "text/plain")
+	rw.Header().Add("charset", "utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("OK"))
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", returnHealth)
 	httpserver := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
